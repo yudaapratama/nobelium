d928b5a0297dc0524e5a769878c65552ade5be51
Yudha Pratama <yudhaprathamaa@gmail.com>
2022-12-26 11:38:26 +0700

    update

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://linkedin.com/in/yudaa-pratama',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
